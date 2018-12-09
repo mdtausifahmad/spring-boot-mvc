cb8d55a3911e387590c7803241114427cdae6fa6
tausif <ahmadmdtausif@gmail.com>
2018-12-09 12:24:31 +0530

    thymeleaf configuration

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spring-boot-mvc
 It is playground for spring mvc
+1. Default configuration can be overriden in `application.properties` file
+
+2. Configuration required to package application as war file
+    
+    a. pom.xml changes
+    
+        1. change packaging from jar to war
+            <packaging>war</packaging>
+            
---
+++ src/main/java/com/tausif/springbootmvc/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.tausif.springbootmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "hello";
+    }
+}
